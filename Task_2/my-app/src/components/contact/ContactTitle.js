9b3fab3a351ddd09d09a1197a8078eab072f6a7e
c11tadel <leeroystay@gmail.com>
2025-03-05 16:45:34 +0200

    add ContactTitle for the "contact" page

---
+++ Task_2/my-app/src/components/contact/ContactTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContactTitle() {
+  return (
+    <div className="contact__title">
+      <h2>Contact Us</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+    </div>
+  );
+}
+
+export default ContactTitle;
