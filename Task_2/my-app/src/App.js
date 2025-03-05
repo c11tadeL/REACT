1fd6138aebba217d0668e94ae43a7e1e5920d815
c11tadel <leeroystay@gmail.com>
2025-03-05 14:33:40 +0200

    Update: in function App added page navigation

--- Task_2/my-app/src/App.js
+++ Task_2/my-app/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/home/Header';
-import './styles/style_home.css';
+import Footer from './components/home/Footer';
+
 import HeroSection from './components/home/HeroSection';
 import HowWeWork from './components/home/HowWeWork';
 import Features from './components/home/Features';
 import Testimonials from './components/home/Testimonials';
 import Faq from './components/home/Faq';
 import Contact from './components/home/Contact';
-import Footer from './components/home/Footer';
+
+import AboutHero from './components/about/AboutHero';
+import WhoWeAre from './components/about/WhoWeAre';
+import ProcessFollow from './components/about/ProcessFollow';
+import MissionVision from './components/about/MissionVision';
+import Benefits from './components/about/Benefits';
+import Team from './components/about/Team';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const navigateTo = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <>
-      <div className="wrapper">
-        <Header />
-        <HeroSection />
+      <div className='wrapper'>
+        <Header 
+          activeLink={currentPage} 
+          onNavigate={navigateTo} 
+        />
       </div>
-      <HowWeWork />
-      <Features />
-      <Testimonials />
-      <Faq />
-      <Contact />
+      
+      {currentPage == 'home' && (
+        <>
+          <div className="wrapper">
+            <HeroSection />
+          </div>
+          <HowWeWork />
+          <Features />
+          <Testimonials />
+          <Faq />
+          <Contact />
+        </>
+      )}
+
+      {currentPage == 'about' && (
+        <>
+          <AboutHero />
+          <WhoWeAre />
+          <ProcessFollow />
+          <MissionVision />
+          <Benefits />
+          <Team />
+        </>
+      )}
+
       <Footer />
     </>
   );
