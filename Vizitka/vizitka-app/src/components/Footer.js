f1a94fe0b8bcbe81a949dc3b34e4309f55026291
c11tadel <leeroystay@gmail.com>
2025-03-09 11:10:14 +0200

    Update : add link for social networks

--- Vizitka/vizitka-app/src/components/Footer.js
+++ Vizitka/vizitka-app/src/components/Footer.js
@@ -8,17 +8,17 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-top-border"></div>
       <div className="footer-content">
-        <div className="social-icons">
-          <a href="#" className="social-icon">
-            <a href="#"><img src={iconInstagram} alt="Instagram" /></a>
-          </a>
-          <a href="#" className="social-icon">
-            <a href="#"><img src={iconTelegram} alt="Telegram" /></a>
-          </a>
-          <a href="#" className="social-icon">
-            <a href="#"><img src={iconGitHub} alt="GitHub" /></a>
-          </a>
-        </div>
+      <div className="social-icons">
+        <a href="https://www.instagram.com/____oscar.rr____" className="social-icon" target="_blank" rel="noopener noreferrer">
+            <img src={iconInstagram} alt="Instagram" />
+        </a>
+        <a href="https://t.me/osccaaarrr" className="social-icon" target="_blank" rel="noopener noreferrer">
+            <img src={iconTelegram} alt="Telegram" />
+        </a>
+        <a href="https://github.com/c11tadeL/REACT/tree/main/Vizitka/vizitka-app" className="social-icon" target="_blank" rel="noopener noreferrer">
+            <img src={iconGitHub} alt="GitHub" />
+        </a>
+    </div>
         <p className="copyright">© Oleksandr Popilevych 2025</p>
       </div>
     </footer>
