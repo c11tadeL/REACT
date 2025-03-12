ee54097b33318f728f1c1cf9c6b8b0e5b25ad548
c11tadel <leeroystay@gmail.com>
2025-03-12 14:28:09 +0200

    add component ColorBox

---
+++ Task_3/color-app/src/components/ColorBox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import calculateLuminance from '../utils/calculateLuminance';
+import '../styles/style.css';
+
+const ColorBox = ({ color, onSelect }) => {
+  const isDark = calculateLuminance(color.color) <= 0.5;
+
+  return (
+    <div 
+      className="color-box" 
+      style={{ backgroundColor: color.color }}
+      onClick={onSelect}
+    >
+      <div className={`color-box-content ${isDark ? 'light-text' : 'dark-text'}`}>
+        <span className="color-name">{color.name}</span>
+        <span className="color-value">{color.color}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ColorBox;
